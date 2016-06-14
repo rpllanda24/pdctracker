aed1ac624afd81a47d33949e69d9d88280489a27
omar.j.p.filoteo <omar.j.p.filoteo@accenture.com>
2016-06-14 19:09:06 +0800

    update UI

--- aswhcm-tracker/src/webapp/aswhcm-tracker-ui/js/app.js
+++ aswhcm-tracker/src/webapp/aswhcm-tracker-ui/js/app.js
@@ -1,17 +1,22 @@
-
 // declare modules
 angular.module('Login', []);
 angular.module('Home', []);
 
-angular.module('aswhcm-tracker', ['ngRoute','Login'])
+angular.module('aswhcm-tracker', [ 'ngRoute', 'Login' ])
 
+.config(function($routeProvider) {
+	$routeProvider
+	.when('/login', {
+		controller : 'LoginController',
+		templateUrl : "aswhcm-tracker-ui/modules/authentication/login.html"
+	})
+	.when('/home', {
+		templateUrl : 'aswhcm-tracker-ui/modules/home/home.html'
+	}).otherwise({
+		redirectTo : '/login'
+	});
+	
 
-.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller: 'LoginController',
-      templateUrl: "aswhcm-tracker-ui/modules/authentication/login.html"
-    })
-
-    .otherwise({ redirectTo: '/' });
+	
+	
 });
