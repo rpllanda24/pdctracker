774afec32037fbeb10580c0f3eefc32e158cff01
omar.j.p.filoteo <omar.j.p.filoteo@accenture.com>
2016-06-14 15:48:45 +0800

    added controller

--- aswhcm-tracker/src/webapp/aswhcm-tracker-ui/modules/authentication/LoginController.js
+++ aswhcm-tracker/src/webapp/aswhcm-tracker-ui/modules/authentication/LoginController.js
@@ -1,1 +1,25 @@
-app.controller('LoginController', []);
+angular.module('Login')
+.controller('LoginController', function($scope){
+	$scope.username = "test";
+	$scope.password = "test";
+
+	$scope.count = 0;
+	
+	$scope.SendData = function(){
+		var data = $.param({
+			userId : $scope.username,
+			passw : $scope.password
+		});
+		
+		var config = {
+				headers : {
+					'Content-Type':'application/json',
+					'Accept':'application/json'
+				}
+		}
+		
+
+		$scope.count = $scope.count + 1;;
+	};
+	
+});
